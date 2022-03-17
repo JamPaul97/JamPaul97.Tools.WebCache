4d140ab4019acae7e38a63c66ee217c52b74b81f
JamPaul-Laptop <mantz@LAPTOP-06D32MGO>
2022-03-17 11:37:09 +0200

    changes

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,16 +1,51 @@
-﻿using System;
+﻿using JamPaul97.Tools.WebCache;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tests
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			var name = Console.ReadLine();
-		}
-	}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cacher = new MemoryCache("directory_to_Save_files");
+            if (cacher.TryCacheBytes("https://ip-fast.com/api/ip/", out byte[] data))
+            {
+                Image img = Image.FromStream(new MemoryStream(data));
+            }
+
+
+
+        }
+        class PayloadObject
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+        public string MakeHTTPRequest(string url, object payload)
+        {
+            var _payload = payload as PayloadObject;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(_payload);
+                streamWriter.Write(json);
+            }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
 }
